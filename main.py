a1484bb416caeadcfd322342e10025e90c1a5de4
Txxxthless <lepash85@gmail.com>
2022-10-04 12:51:02 +0300

    updated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import random
 import requests
+import time
 from telebot import types
 
 bot = telebot.TeleBot('5715762546:AAFOrpIoGNKEmx2_qHi-P1z95-pTGviuOYs')
@@ -8,16 +9,19 @@ bot = telebot.TeleBot('5715762546:AAFOrpIoGNKEmx2_qHi-P1z95-pTGviuOYs')
 
 @bot.message_handler(commands=['start'])
 def start(msg):
-    message = f'<b>✋ Привіт, {msg.from_user.first_name}.</b>'
+    message = f'<b>Привіт, {msg.from_user.first_name}.</b>'
     bot.send_message(msg.chat.id, message, parse_mode='html')
-    list = f'<b>Для ознайомлення із функціоналом натисніть "Допомога" !</b>'
+    list = f'<b>Для ознайомлення із функціоналом введіть команду /help !</b>'
     bot.send_message(msg.chat.id, list, parse_mode='html')
 
 @bot.message_handler(commands=['help'])
 def help(msg):
         message = f'<b>Я можу виконувати такі команди: </b>'
         bot.send_message(msg.chat.id, message, parse_mode='html')
-        msgList = f'<b>Перелік</b>'
+        msgList = f'<b>/start - початок роботи\n/help - перелік функціоналу' \
+                  f'\n/randompaste - випадково сгенерована фраза' \
+                  f'\n/randomcat - випадкова світлина з кицею' \
+                  f'\n/dota - створення опросу про готовність піти у Dota 2</b>'
         bot.send_message(msg.chat.id, msgList, parse_mode='html')
 
 
@@ -44,6 +48,12 @@ def randomcat(msg):
         message = f'<b>Ось ваша киця:</b>'
         bot.send_message(msg.chat.id, message, parse_mode='html')
         bot.send_photo(msg.chat.id, catPic)
+
+@bot.message_handler(commands=['dota'])
+def dota(msg):
+    message = f'Погнали в дотку?'
+    bot.send_poll(msg.chat.id, message, ["Так!","Ні..."],False, close_date=int(round(time.time() + 3600)),)
+
 
 
 bot.polling(none_stop=True)

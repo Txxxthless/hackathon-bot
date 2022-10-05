9be2b24f66f3ebe68c1f072d2e1f8d56d869de9b
Txxxthless <lepash85@gmail.com>
2022-10-05 11:42:49 +0300

    updated

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 import telebot
 import random
 import requests
@@ -5,6 +7,25 @@ import time
 from telebot import types
 
 bot = telebot.TeleBot('5715762546:AAFOrpIoGNKEmx2_qHi-P1z95-pTGviuOYs')
+
+def randompaste_func():
+    items1 = [f'Я', f'ФПМ', f'Міша', f'Ананас', f'Шевченко']
+    items2 = [' - найкращий ', ' - дивний ', ' - неймовірно старанний ', ' - військовий ', ' - загальновизнаний ']
+    items3 = ['чарівник.', 'геній.', 'кібервійськовий.', 'красень.', 'інопланетянин.']
+    i1 = random.randint(0, 4)
+    i2 = random.randint(0, 4)
+    i3 = random.randint(0, 4)
+    message = items1[i1] + items2[i2] + items3[i3]
+    return message
+
+def randomcatpic():
+    url = 'https://cataas.com/cat'
+    name = 'catpic.jpg'
+    req = requests.get(url)
+    with open(name, 'wb') as f:
+        f.write(req.content)
+    catPic = open('catpic.jpg', 'rb')
+    return catPic
 
 
 @bot.message_handler(commands=['start'])
@@ -29,24 +50,13 @@ def help(msg):
 
 @bot.message_handler(commands=['randompaste'])
 def randompaste(msg):
-        items1 = [f'<b>Я</b>', f'<b>ФПМ</b>', f'<b>Міша</b>', f'<b>Ананас</b>', f'<b>Шевченко</b>']
-        items2 = [' - найкращий ', ' - дивний ', ' - неймовірно старанний ', ' - військовий ', ' - загальновизнаний ']
-        items3 = ['чарівник.','геній.','кібервійськовий.','красень.','інопланетянин.']
-        i1 = random.randint (0, 4)
-        i2 = random.randint (0, 4)
-        i3 = random.randint (0, 4)
-        message = items1[i1] + items2[i2] + items3[i3]
+        message = randompaste_func()
         bot.send_message(msg.chat.id, message, parse_mode='html' )
 
 
 @bot.message_handler(commands=['randomcat'])
 def randomcat(msg):
-        url = 'https://cataas.com/cat'
-        name = 'catpic.jpg'
-        req = requests.get(url)
-        with open(name, 'wb') as f:
-            f.write(req.content)
-        catPic = open('catpic.jpg', 'rb')
+        catPic = randomcatpic()
         message = f'<b>Ось ваша киця:</b>'
         bot.send_message(msg.chat.id, message, parse_mode='html')
         bot.send_photo(msg.chat.id, catPic)
@@ -61,11 +71,54 @@ def forward(msg):
     messageId = random.randint (3, 12)
     bot.forward_message(msg.chat.id, from_chat_id='-1001449056768', message_id=messageId)
 
+
 @bot.message_handler(commands=['rollformid'])
 def rollformid(msg):
     message = f'<b>{msg.from_user.first_name} отримує число від 1 до 100: </b>'
     x = random.randint(1, 100)
     bot.send_message(msg.chat.id, message, parse_mode='html')
     bot.send_message(msg.chat.id, x, parse_mode='html')
+
+@bot.inline_handler(lambda query: query.query == 'фраза')
+def query_text(inline_query):
+    try:
+        message = randompaste_func()
+        result = types.InlineQueryResultArticle('1', 'Випадкова фраза', types.InputTextMessageContent(message))
+        bot.answer_inline_query(inline_query.id, [result], cache_time=1)
+    except Exception as e:
+        print(e)
+
+@bot.inline_handler(lambda query: query.query == 'киця')
+def query_photo(inline_query):
+    try:
+        result = types.InlineQueryResultPhoto('1',
+                                         'https://cdn2.thecatapi.com/images/TdxQ2VvJK.jpg',
+                                         'https://cdn2.thecatapi.com/images/TdxQ2VvJK.jpg',)
+        result2 = types.InlineQueryResultPhoto('2',
+                                          'https://cdn2.thecatapi.com/images/9c6.jpg',
+                                          'https://cdn2.thecatapi.com/images/9c6.jpg')
+        result3 = types.InlineQueryResultPhoto('3',
+                                         'https://cdn2.thecatapi.com/images/9ea.jpg',
+                                         'https://cdn2.thecatapi.com/images/9ea.jpg',)
+        bot.answer_inline_query(inline_query.id, [result, result2, result3], cache_time=1)
+    except Exception as e:
+        print(e)
+
+@bot.inline_handler(lambda query: query.query == 'мгр')
+def query_text(inline_query):
+    try:
+        result = types.InlineQueryResultArticle('1', 'Пояснюю...', types.InputTextMessageContent(
+            'https://media.tenor.com/TZS6fvPcrl0AAAAd/senator-armstrong-armstrong.gif'))
+        result2 = types.InlineQueryResultArticle('2', 'Тобі капець', types.InputTextMessageContent(
+            'https://media.tenor.com/t_OITIPhJTkAAAAC/armstrong-senator-armstrong.gif'))
+        result3 = types.InlineQueryResultArticle('3', 'Погоджуюсь', types.InputTextMessageContent(
+            'https://media.tenor.com/4S6j9gbql6kAAAAd/senator-armstrong-handshake.gif'))
+        bot.answer_inline_query(inline_query.id, [result, result2, result3], cache_time=1)
+    except Exception as e:
+        print(e)
+
+
+
+
 
 bot.polling(none_stop=True)
